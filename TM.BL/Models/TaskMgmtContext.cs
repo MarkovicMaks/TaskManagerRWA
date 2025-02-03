9d0f90ea3069ae7ea1ea02d6632a3925296e95db
MarkovicMaks <markovic.maks@gmail.com>
2025-02-03 14:24:16 +0100

    conection fixes

--- TM.BL/Models/TaskMgmtContext.cs
+++ TM.BL/Models/TaskMgmtContext.cs
@@ -30,9 +30,7 @@ public partial class TaskMgmtContext : DbContext
     public virtual DbSet<UserSkill> UserSkills { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=.\\MSSQLSERVER3;Database=TaskMgmt;User=sa;Password=SQL;TrustServerCertificate=True;MultipleActiveResultSets=true");
-
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:TaskM");
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Manager>(entity =>
